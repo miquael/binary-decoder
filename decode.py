eb12fd09641d1c666d110b03df328e1147d9045f
miquael <m@michaelgaio.com>
2022-09-07 00:25:17 -0700

    first commit

---
+++ decode.py
@@ -0,0 +1,28 @@
+# decode file contents (binary)
+# in terminal, run to decode file (key0):
+# > python3 decode3.py key0
+
+import sys
+
+file = sys.argv[1]
+
+with open(file, 'r') as f:
+    encoded = f.read()
+print(file + ' encoded content: ')
+print(encoded)
+
+# split content string 
+enc = encoded.split(" ")
+
+# dec will hold the decoded string
+dec = ""
+
+# decode
+print('decoded content: ')
+for i in range(len(enc)):
+  dec += chr(int(enc[i], 2))
+print(dec)
+
+# decode lenth (chars)
+length = len(enc)
+print('length: ' + str(length))
